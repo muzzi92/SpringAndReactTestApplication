d702b1666de4f33f0f3f1c7ce40cae0bd140f79b
Possed <r0xana.pirvu@yahoo.com>
2018-06-06 15:56:54 +0100

    muzzi is a silly sausage

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+    entry: './src/main/js/app.js',
+    devtool: 'sourcemaps',
+    cache: true,
+    debug: true,
+    output: {
+        path: __dirname,
+        filename: './src/main/resources/static/build/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: path.join(__dirname, '.'),
+                exclude: /(node_modules)/,
+                loader: 'babel',
+                query: {
+                    cacheDirecory: true,
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    }
+};
